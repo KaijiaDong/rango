a21cdd5c45d93304e359118cbf8957a4f2714a07
董恺嘉 <Kevin@dongkaijiadeMacBook-Pro.local>
2017-02-07 00:43:52 +0000

    chapter8 without exercise

--- rango/tests.py
+++ rango/tests.py
@@ -1,14 +1,32 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
-
-
 from rango.models import Category
+
 def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
     c.views = views
     c.likes = likes
     c.save()
     return c
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        ensure_views_are_positive should results True for categories where views are zero or positive
+        """
+        cat = Category(name='test',views=-1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), True)
+
+    def test_slug_line_creation(self):
+        """
+            slug_line_creation checks to make sure that when we add
+            a category an appropriate slug line is created
+            i.e. "Random Category String" -> "random-category-string"
+            """
+        cat = Category(name='Random Category String')
+        cat.save()
+        self.assertEqual(cat.slug, 'random-category-string')
 
 class IndexViewTests(TestCase):
     def test_index_view_with_no_categories(self):
