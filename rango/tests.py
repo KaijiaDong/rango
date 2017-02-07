da251055c1dcec873f1f298b143875f79b272787
董恺嘉 <Kevin@dongkaijiadeMacBook-Pro.local>
2017-02-07 00:33:28 +0000

    chapter 8 test

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+from rango.models import Category
+def add_cat(name, views, likes):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+class IndexViewTests(TestCase):
+    def test_index_view_with_no_categories(self):
+        """
+        If no questions exist, an appropriate message should be displayed.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+    def test_index_view_with_categories(self):
+        """
+        Check to make sure that the index has categories displayed
+        """
+        add_cat('test', 1, 1)
+        add_cat('temp', 1, 1)
+        add_cat('tmp', 1, 1)
+        add_cat('tmp test temp', 1, 1)
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "tmp test temp")
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
